93acdada4a6b7f8ad98fd2406193e45e6b3b695e
Ricardo Rosas <ricardo.rosasv@gmail.com>
2022-06-30 11:56:00 +0200

    Revert "Revert "Revert "Revert "Revert "'Mods_para_iniciar_produccion'"""""
    This reverts commit 89be7f9658da49249af28eff531d386c0562d796.

--- rrhh/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rrhh.models import Empleado, Area_RH, Capacitacion, Plan_Cap, Ejecuta_Cap
-# Register your models here.
-
-admin.site.register(Empleado)
-admin.site.register(Area_RH)
-admin.site.register(Capacitacion)
-admin.site.register(Plan_Cap)
-admin.site.register(Ejecuta_Cap)
